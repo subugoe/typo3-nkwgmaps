3e2274acf383ea8058d6ce01b0859a8a055fef08
simm <simm@sub.uni-goettingen.de>
2010-05-11 13:22:42 +0000

    ext:nkwgmaps - multi address entry map
    git-svn-id: https://develop.sub.uni-goettingen.de/repos/w3d/branches/dev/typo3conf/ext/nkwgmaps@373 f004c202-0a6e-0410-85e4-8d7485fa9ffe

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,5 +3,5 @@ if (!defined ('TYPO3_MODE')) die ('Access denied.');
 t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_nkwgmaps_pi1.php', '_pi1', 'list_type', 1);
 t3lib_extMgm::addPItoST43($_EXTKEY, 'pi2/class.tx_nkwgmaps_pi2.php', '_pi2', 'list_type', 1);
 t3lib_extMgm::addPItoST43($_EXTKEY, 'pi3/class.tx_nkwgmaps_pi3.php', '_pi3', 'list_type', 1);
-t3lib_extMgm::addPItoST43($_EXTKEY, 'pi3/class.tx_nkwgmaps_pi4.php', '_pi4', 'list_type', 1);
+t3lib_extMgm::addPItoST43($_EXTKEY, 'pi4/class.tx_nkwgmaps_pi4.php', '_pi4', 'list_type', 1);
 ?>
