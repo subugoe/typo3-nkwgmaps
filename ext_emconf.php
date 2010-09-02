f259dbbe7e231ca3559a312f3a05507d5d9e44d1
windisch <windisch@sub.uni-goettingen.de>
2010-09-02 08:44:57 +0000

    git-svn-id: https://develop.sub.uni-goettingen.de/repos/w3d/branches/dev/typo3conf/ext/nkwgmaps@614 f004c202-0a6e-0410-85e4-8d7485fa9ffe

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,26 +1,15 @@
 <?php
-/***************************************************************
-*  Copyright notice
-*
-*  (c) 2010 Nils K. Windisch <windisch@sub.uni-goettingen.de>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+
+########################################################################
+# Extension Manager/Repository config file for ext "nkwgmaps".
+#
+# Auto generated 01-09-2010 07:58
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Google Maps',
 	'description' => '',
@@ -40,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'Goettingen State and University Library, Germany http://www.sub.uni-goettingen,de',
-	'version' => '0.0.5',
+	'version' => '0.0.6',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -49,6 +38,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"4e72";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"a5f9";s:14:"ext_tables.php";s:4:"4e6d";s:16:"locallang_db.xml";s:4:"74da";s:19:"doc/wizard_form.dat";s:4:"05a5";s:20:"doc/wizard_form.html";s:4:"7297";s:29:"pi1/class.tx_nkwgmaps_pi1.php";s:4:"dcf4";s:17:"pi1/locallang.xml";s:4:"5c13";}',
+	'_md5_values_when_last_written' => 'a:21:{s:10:"README.txt";s:4:"d41d";s:12:"ext_icon.gif";s:4:"c8ca";s:17:"ext_localconf.php";s:4:"f344";s:14:"ext_tables.php";s:4:"31d5";s:13:"locallang.xml";s:4:"a105";s:16:"locallang_db.xml";s:4:"34b5";s:19:"doc/wizard_form.dat";s:4:"05a5";s:20:"doc/wizard_form.html";s:4:"7297";s:25:"lib/class.tx_nkwgmaps.php";s:4:"0da5";s:29:"pi1/class.tx_nkwgmaps_pi1.php";s:4:"1c5e";s:16:"pi1/flexform.xml";s:4:"a2ac";s:17:"pi1/locallang.xml";s:4:"f3a9";s:29:"pi2/class.tx_nkwgmaps_pi2.php";s:4:"53d0";s:16:"pi2/flexform.xml";s:4:"c617";s:17:"pi2/locallang.xml";s:4:"2fd2";s:29:"pi3/class.tx_nkwgmaps_pi3.php";s:4:"9419";s:16:"pi3/flexform.xml";s:4:"47ee";s:17:"pi3/locallang.xml";s:4:"2fd2";s:29:"pi4/class.tx_nkwgmaps_pi4.php";s:4:"54f8";s:16:"pi4/flexform.xml";s:4:"6f85";s:17:"pi4/locallang.xml";s:4:"2fd2";}',
+	'suggests' => array(
+	),
 );
+
 ?>
